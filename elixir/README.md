daed989d6122f0504ea51c32d87e444bebed5685
Luiz Baldi <baldilp@gmail.com>
2020-01-20 21:34:04 -0300

    [elixir] Pattern matching and pipe operator examples

---
+++ elixir/README.md
@@ -0,0 +1,9 @@
+# Studies on Elixir
+
+Folder created to store my studies on Elixir Lang :)
+
+---
+
+So far, the courses have been:
+
+- [The Complete Elixir and Phoenix Bootcamp](https://www.udemy.com/course/the-complete-elixir-and-phoenix-bootcamp-and-tutorial/)
---
+++ elixir/elixir-and-phoenix-bootcamp/cards/lib/cards.ex
@@ -0,0 +1,41 @@
+defmodule Cards do
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for value <- values, suit <- suits do
+      "#{value} of #{suit}"
+    end
+
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary deck
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _} -> "Ops, an error has ocurred"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+      |> Cards.shuffle
+      |> Cards.deal(hand_size)
+  end
+end
