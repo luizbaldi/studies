6607c03c32b92c3150816b9d6a8cc374bc1a296d
Luiz Baldi <baldilp@gmail.com>
2020-01-21 21:53:14 -0300

    [elixir] Add testing and docs examples

--- elixir/elixir-and-phoenix-bootcamp/cards/lib/cards.ex
+++ elixir/elixir-and-phoenix-bootcamp/cards/lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    Returns a list of strings representing a deck of cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -13,10 +20,28 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card
+
+    ## Examples
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in the hand.
+
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> {hand, _} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
--- elixir/elixir-and-phoenix-bootcamp/cards/test/cards_test.exs
+++ elixir/elixir-and-phoenix-bootcamp/cards/test/cards_test.exs
@@ -2,7 +2,12 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "should create a deck with 20 cards" do
+    assert length(Cards.create_deck) == 20
+  end
+
+  test "should shuffle deck properly" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
   end
 end
