620cca6d462f5c01a39eba67e739158bfdb9db99
Luiz Baldi <baldilp@gmail.com>
2020-09-28 07:51:15 -0300

    [elixir] Use code generators to generate a lot of stuff _(it even generate tests)_

--- elixir/programming-phoenix-1.4-book/rumbl/lib/rumbl_web/controllers/auth.ex
+++ elixir/programming-phoenix-1.4-book/rumbl/lib/rumbl_web/controllers/auth.ex
@@ -1,5 +1,8 @@
 defmodule RumblWeb.Auth do
   import Plug.Conn
+  import Phoenix.Controller
+
+  alias RumblWeb.Router.Helpers, as: Routes
 
   def init(opts), do: opts
 
@@ -18,5 +21,16 @@ defmodule RumblWeb.Auth do
 
   def logout(conn) do
     configure_session(conn, drop: true)
+  end
+
+  def authenticate_user(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt
+    end
   end
 end
